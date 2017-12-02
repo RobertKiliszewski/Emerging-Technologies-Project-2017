73bbde7d86f5493ce14a1a9ca813aed969a74c30
Robert Kiliszewski <robert.kiliszewski@hotmail.com>
2017-12-02 20:46:59 +0000

    File upload

--- script.py
+++ script.py
@@ -1,13 +1,22 @@
-import flask as fl
-from flask import render_template, g, request 
+import os 
+from flask import render_template, Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-app = fl.Flask(__name__)
+app = Flask(__name__)
+UPLOAD_FOLDER = os.path.basename('./images')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 @app.route("/")
+def index():
+     return render_template("index.html")
 
-def name():
-	return render_template("Index.html")
-	
-	
-if __name__ == "__main__":
-	app.run()
+@app.route('/uploader', methods=['POST'])
+def upload_file():
+	f = request.files['image']
+	tmp = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+	f.save(tmp)
+	return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = True)
