53885798ef9fe30e99dfe24df5cab22a0faa311a
Robert Kiliszewski <robert.kiliszewski@hotmail.com>
2017-12-03 15:08:37 +0000

    Canvas

--- script.py
+++ script.py
@@ -3,15 +3,19 @@ from flask import render_template, Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
 UPLOAD_FOLDER = os.path.basename('./images')
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 @app.route("/")
 def index():
      return render_template("index.html")
 
-@app.route('/uploader', methods=['POST'])
+
+@app.route('/uploadFile', methods=['POST'])
 def upload_file():
 	f = request.files['image']
 	tmp = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
